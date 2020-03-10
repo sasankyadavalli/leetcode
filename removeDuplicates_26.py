5ce0410d490345f5976ffddf1d81820c6889b248
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-03-10 21:24:39 +0530

    Remove duplicates from Sorted Array_LC26

---
+++ removeDuplicates_26.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        for i in range(len(nums)-1, 0, -1):
+            if(nums[i] == nums[i-1]):
+                del nums[i]
+                
+        print(nums)
