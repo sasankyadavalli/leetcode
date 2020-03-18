e895583c1f0b33580fed72713ffb168a167723f0
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-03-18 21:56:28 +0530

    Missing Number-LC268

---
+++ missingNumber_268.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        nums.sort()
+        for i in range(len(nums)):
+            if(nums[i] != (i)):
+                return i
+        return i+1
