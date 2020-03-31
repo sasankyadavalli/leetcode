3af4f8b300dafaf16203a5db0c81b61eb1fc1104
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-03-31 17:14:29 +0530

    Longest Common Prefix - LC 14

---
+++ longestCommonPrefix_14.py
@@ -0,0 +1,19 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        if len(strs) == 0:
+            return ""
+        if len(strs) == 1:
+            return strs[0]
+        
+        strs.sort()
+
+        end = min(len(strs[0]), len(strs[len(strs)-1]))
+        
+        i = 0
+        
+        while(i < end and strs[0][i] == strs[len(strs)-1][i]):
+            i += 1
+            
+        prefix = strs[0][0:i]
+        return prefix
+
