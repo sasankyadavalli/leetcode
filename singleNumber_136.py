2a999a1c549bbdec7eea182c77158e2cb37e3c8b
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-04-03 03:46:31 +0530

    single Number(elegent solution)-LC136

--- singleNumber_136.py
+++ singleNumber_136.py
@@ -1,12 +1,18 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        d = {}
-        for ele in nums:
-            if ele in d.keys():
-                d[ele] += 1
-            else:
-                d[ele] = 1
+        xor = 0
+        for i in nums:
+            xor ^= i
+        return xor
+        
+        
+        # d = {}
+        # for ele in nums:
+        #     if ele in d.keys():
+        #         d[ele] += 1
+        #     else:
+        #         d[ele] = 1
                 
-        for k, v in d.items():
-            if v % 2 != 0:
-                return k
+        # for k, v in d.items():
+        #     if v % 2 != 0:
+        #         return k
