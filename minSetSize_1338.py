5ae61ca48401abc76588b3b00292b33c7b7a69f7
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-04-18 21:01:03 +0530

    Reduce Array Size to The Half-LC1338

---
+++ minSetSize_1338.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minSetSize(self, arr: List[int]) -> int:
+        d = {}
+        count = 0
+        for ele in arr:
+            if ele in d.keys():
+                d[ele] += 1
+            else:
+                d[ele] = 1
+                
+        d = sorted(d.items(), key=lambda x:x[1] ,reverse = True)
+       
+        a = len(arr)
+        for k, v in d:
+            a -= v
+            count += 1
+            
+            if a <= len(arr)//2:
+                break
+                
+        return count
