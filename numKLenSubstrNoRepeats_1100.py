1257f8a32205c66b171ac8184bcedc3ca07c8070
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-05-13 22:32:48 +0530

    Find K-Length Substrings With No Repeated Characters-LC1100

--- numKLenSubstrNoRepeats_1100.py
+++ numKLenSubstrNoRepeats_1100.py
@@ -3,9 +3,6 @@ class Solution:
         if len(S) < K:
             return 0
         count = 0
-        window = S[:K]
-        if len(window) == len(set(window)):
-            count += 1
         for i in range(1, len(S)-K+1):
             window = S[i:K+i]
             if len(window) == len(set(window)):
