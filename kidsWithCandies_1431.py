7d2ebb6186e73c169b955e4dec963b22a74f03ea
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-05-16 20:46:16 +0530

    Kids With the Greatest Number of Candies-LC1431

---
+++ kidsWithCandies_1431.py
@@ -0,0 +1,11 @@
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        res = []
+        a = max(candies)
+        for i in range(len(candies)):
+            if candies[i] + extraCandies >= a:
+                res.append(True)
+            else:
+                res.append(False)
+                
+        return res
