b0acbe9947cc7c236d63abe5b1d881ca16b6ee9b
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-06-26 19:28:26 +0530

    Implement Queue using Stacks-LC232

---
+++ myQueue_232.py
@@ -0,0 +1,54 @@
+class MyQueue:
+
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.s1 = []
+        self.s2 = []
+        self.top = None
+    def push(self, x: int) -> None:
+        """
+        Push element x to the back of queue.
+        """
+        self.s1.append(x)
+        self.top = self.s1[0]
+
+    def pop(self) -> int:
+        """
+        Removes the element from in front of queue and returns that element.
+        """
+        while(len(self.s1) != 0):
+            self.s2.append(self.s1.pop())
+
+        if len(self.s2) != 0:
+            pop = self.s2.pop()
+            if len(self.s2) != 0:
+                self.top = self.s2[-1]
+
+        while(len(self.s2) != 0):
+            self.s1.append(self.s2.pop())
+
+        return pop
+    def peek(self) -> int:
+        """
+        Get the front element.
+        """
+        return self.top
+
+    def empty(self) -> bool:
+        """
+        Returns whether the queue is empty.
+        """
+        if len(self.s1) == 0:
+            return True
+        else:
+            return False
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
