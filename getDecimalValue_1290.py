f9a4f2f7e0313bd4e960a5f70b4d6b15a22ed5d2
Sasank Yadavalli <yadavallisasank@gmail.com>
2020-06-29 21:24:35 +0530

    Convert Binary Number in a Linked List to Integer-LC1290

---
+++ getDecimalValue_1290.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def getDecimalValue(self, head: ListNode) -> int:
+        s = ''
+        cur = head
+        while cur:
+            s += str(cur.val)
+            cur = cur.next
+
+        return int(s, 2)
